0017532f82fc460c918dc61274c72f395d1e9e11
EC2 Default User <ec2-user@ip-172-31-2-236.ap-northeast-1.compute.internal>
2025-08-18 10:10:47 +0000

    [add] comment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :group, optional: true
+  has_many :comments, dependent: :destroy
 
   validates :content, presence: true, length:{maximum:20}
 end
